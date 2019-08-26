e7d48d71a32d8abc76f353db87ba5ebad5042091
Rebecca Oei <e0322808@u.nus.edu>
2019-08-26 16:44:05 +0800

    Level 3 Completed

--- src/main/java/Tasklist.java
+++ src/main/java/Tasklist.java
@@ -1,6 +1,6 @@
 public class Tasklist {
     private String[] tasklist = new String[100];
-
+    private boolean[] donelist = new boolean[100];
     private int listIndex = 0;
 
     private void setListIndex(int value) {
@@ -12,6 +12,7 @@ public class Tasklist {
 
     private void addListItem(String item) {
         tasklist[getListIndex()] = item; //Update the right index
+        donelist[getListIndex()] = false;
         System.out.println("Item added: " + item);
         setListIndex(getListIndex() + 1); //Increment the index
     }
@@ -19,14 +20,29 @@ public class Tasklist {
     private void printList() {
         int max = getListIndex();
         for (int i = 0; i < max; i++) {
-            System.out.println((i+1) + ". " + tasklist[i]);
+            if (donelist[i]) {
+                System.out.print("[\u2713]");
+            } else {
+                System.out.print("[\u2718]");
+            }
+            System.out.println(" " + (i+1) + ". " + tasklist[i]);
         }
+    }
+
+    private void markAsDone(int i) {
+        System.out.print("Nice! I've marked this task as done: ");
+        donelist[i] = true; //Mark task as done.
+        System.out.println(tasklist[i]); //Prints taskname
     }
 
     public void handleListInput(String listInput) {
         if (listInput.equals("list")) {
             printList();
-        } else {
+        } else if (listInput.length() >= 4 && listInput.substring(0, 4).equals("done")){
+            String number = listInput.substring(5);
+            //System.out.println("done placeholder");
+            markAsDone(Integer.parseInt(number)-1);
+        }else {
             addListItem(listInput);
         }
     }
